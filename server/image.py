60a303a35fd8721e15026eb8432ece75bcfb9116
lucy7li <lucy7li@gmail.com>
2014-11-15 02:42:44 -0500

    slsdkljf

--- server/image.py
+++ server/image.py
@@ -1,6 +1,15 @@
 import os, sys
 from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
 
 rageFace = Image.open("images/troll.jpg")
+
+#font = ImageFont.truetype("~/../usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 25)
+
+draw = ImageDraw.Draw(rageFace)
+draw.text((10,10), "Troll" ,(0,0,0))#, font=font)
+draw = ImageDraw.Draw(rageFace)
+rageFace.save("test.jpg")
 
 
